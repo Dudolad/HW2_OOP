86a9e52d8d142071c2cb21862b2814c7354aca79
dudolad_yaroslav <dudolad2009@gmail.com>
2025-07-26 18:31:11 +0300

    Created "Tour.h"

---
+++ HW2_OOP/Tour.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Tour
+{
+private:
+	char* m_name;
+	char* m_destination;
+	float m_ricePerDay;
+	int m_days;
+	char m_meals;
+	float m_transportCost;
+public:
+	Tour();
+	Tour(const char* name, const char* destination, float pricePerDay, int days, const char* meals, float transportCost);
+	Tour(const Tour& other);
+	Tour& operator = (const Tour& other);
+	~Tour();
+
+	const char* getName() const;
+	const char* getDestination() const;
+	float getPricePerDay() const;
+	int getDays() const;
+	const char* getMeals() const;
+	float getTransportCost() const;
+
+	void showInfo() const;
+};
+
